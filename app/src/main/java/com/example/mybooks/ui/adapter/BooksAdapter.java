200e796bf2bb973961d0929f39a79c1d3c99c2f4
Hugo <hugoribeirorneto@gmail.com>
2025-10-14 21:29:39 -0300

    Ajustado layout do item_book e feitos comentários no código a fim de estudo/compreensão

--- app/src/main/java/com/example/mybooks/ui/adapter/BooksAdapter.java
+++ app/src/main/java/com/example/mybooks/ui/adapter/BooksAdapter.java
@@ -13,10 +13,15 @@ import com.example.mybooks.ui.viewHolder.BookViewHolder;
 import java.util.ArrayList;
 import java.util.List;
 
+//O Adapter é a ponte entre o dado e a view, responsável também por criar uma view para cada item no dataset
+
 public class BooksAdapter extends RecyclerView.Adapter<BookViewHolder> {
 
+    //Cria uma lista de bookentity
     private List<BookEntity> booksList = new ArrayList<>();
 
+
+    //Infla a viewholder, ou seja, instancia ela, da vida a viewholder
     @NonNull
     @Override
     public BookViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -24,18 +29,23 @@ public class BooksAdapter extends RecyclerView.Adapter<BookViewHolder> {
         return new BookViewHolder(view);
     }
 
+    //É o que faz a passagem do dataset e binda a viewholder, no caso bindando a BookViewHolder, o
+    //position é a posição do dado que deve ser passado, como é uma lista ele começa em 0 e vai atualizando
+    //a cada novo dado que deve ser passado
     @Override
     public void onBindViewHolder(@NonNull BookViewHolder holder, int position) {
         holder.bind(booksList.get(position));
     }
 
+    //Número de itens na lista
     @Override
     public int getItemCount() {
         return booksList.size();
     }
 
+    //Traz os livros que estäo na bookentity e vincula a lista criada aqui
+    //Essa atualização é feita lá no observer do fragment
     public void updateBooks(List<BookEntity> books){
         booksList = books;
-        notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/example/mybooks/ui/viewHolder/BookViewHolder.java
+++ app/src/main/java/com/example/mybooks/ui/viewHolder/BookViewHolder.java
@@ -1,15 +1,22 @@
 package com.example.mybooks.ui.viewHolder;
 
+import android.graphics.drawable.Drawable;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.mybooks.R;
 import com.example.mybooks.databinding.ItemBookBinding;
 import com.example.mybooks.entity.BookEntity;
 
+//A ViewHolder é a view a qual o adapter vai utilizar para demonstrar os dados do dataset
+//Aqui é onde os dados são manipulados VISUALMENTE
+
 public class BookViewHolder extends RecyclerView.ViewHolder {
 
+    //Criado item_book.xml que é a view que vamos utilizar para manipular e apresentar os dados do dataset
+    //Após criação instanciamos/bindamos ele aqui na viewholder
     private final ItemBookBinding item;
 
     public BookViewHolder(@NonNull ItemBookBinding itemView) {
@@ -17,7 +24,16 @@ public class BookViewHolder extends RecyclerView.ViewHolder {
         this.item = itemView;
     }
 
+    //Binda a bookentity a essa view, fazendo com que o que está lá possa ser replicado aqui
     public void bind(BookEntity book){
         item.textviewTitle.setText(book.getTitle());
+        item.textviewGenre.setText(book.getGenre());
+        item.textviewAuthor.setText(book.getAuthor());
+
+        if (book.isFavorite()){
+            item.imageviewFavorite.setImageResource(R.drawable.ic_favorite);
+        }else{
+            item.imageviewFavorite.setImageResource(R.drawable.ic_favorite_empty);
+        }
     }
 }
